0a6176206402a11a8b6b2c1a3f5046c9d9175cbf
jose viejo <jviejo.devtoday@gmail.com>
2023-08-10 17:22:34 +0200

    test

---
+++ funciones.test.js
@@ -0,0 +1,21 @@
+const { suma, resta, isPar, getArray } = require('./funciones');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(suma(1, 2)).toBe(3);
+});
+
+test('resta 5 - 2 to equal 3', () => {
+    expect(resta(5, 2)).toBe(3);
+});
+
+test('test if 5 not isPar', () => {
+    expect(isPar(5)).toBe(false);
+});
+
+test('test if 6 isPar', () => {
+    expect(isPar(6)).toBe(true);
+});
+
+test('test if getArray nos devuelve 1,2,3', () => {
+    expect(getArray(1, 3)).toStrictEqual([1,2,3]);
+});
